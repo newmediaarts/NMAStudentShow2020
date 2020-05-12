c27b1ce9ec518dc809554ca75b290f1666a33cd9
tomytak <tomyt@hawaii.edu>
2020-05-12 12:30:49 -1000

    adds coming soon

--- dist/fog.03cabc1b.js
+++ dist/fog.03cabc1b.js
@@ -184,7 +184,7 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"scss/styles.scss":[function(require,module,exports) {
+},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"fog.scss":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
@@ -394,4 +394,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/styles.5c8f3eab.js.map
+//# sourceMappingURL=/fog.03cabc1b.js.map
