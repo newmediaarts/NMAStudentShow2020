779e699775f19a640e52a75fd6187e31c54d5fe7
tomytak <tomyt@hawaii.edu>
2020-05-18 16:36:36 -1000

    added img folder. formatted coming soon page for hero for chris. needs line inbetween 2 dates

--- dist/scripts.cd665a19.js
+++ dist/scripts.cd665a19.js
@@ -294,7 +294,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49173" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49190" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
