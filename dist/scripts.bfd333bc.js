286361df0c74b2146290a8019903b236e2e34458
lummelis <lummelis@hawaii.edu>
2020-05-21 20:06:26 -1000

    update imgs

---
+++ dist/scripts.bfd333bc.js
@@ -0,0 +1,100 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"HvU3":[function(require,module,exports) {
+module.exports="andruw1.3c30dd35.jpg";
+},{}],"i341":[function(require,module,exports) {
+module.exports="andruw2.909c0e6b.jpg";
+},{}],"KSi4":[function(require,module,exports) {
+module.exports="annika1.19874dfc.jpg";
+},{}],"xiDP":[function(require,module,exports) {
+module.exports="annika2.606a2f20.jpg";
+},{}],"lH4H":[function(require,module,exports) {
+module.exports="ariana1.4a614216.jpg";
+},{}],"FJiR":[function(require,module,exports) {
+module.exports="ariana2.8e3f0579.jpg";
+},{}],"p9dy":[function(require,module,exports) {
+module.exports="background.ebfc8e15.jpg";
+},{}],"WIW0":[function(require,module,exports) {
+module.exports="blue_fairy.c41fc81b.jpg";
+},{}],"RYUG":[function(require,module,exports) {
+module.exports="christi1.ec19c658.jpg";
+},{}],"mee9":[function(require,module,exports) {
+module.exports="christi2.d4eab2f3.jpg";
+},{}],"ZLX6":[function(require,module,exports) {
+module.exports="christy1.3ff1a4c8.jpg";
+},{}],"FKNm":[function(require,module,exports) {
+module.exports="emily1.5091bb2a.jpg";
+},{}],"faKY":[function(require,module,exports) {
+module.exports="christy2.973d44a4.jpg";
+},{}],"gkec":[function(require,module,exports) {
+module.exports="emily2.f93a3094.jpg";
+},{}],"x0jt":[function(require,module,exports) {
+module.exports="eryn1.222e612f.jpg";
+},{}],"i0yU":[function(require,module,exports) {
+module.exports="eryn2.714809ff.jpg";
+},{}],"Hdkz":[function(require,module,exports) {
+module.exports="evan1.53202316.jpg";
+},{}],"Tree":[function(require,module,exports) {
+module.exports="evan2.ecaf6925.jpg";
+},{}],"YPVS":[function(require,module,exports) {
+module.exports="fog-1.21bb2e3f.jpg";
+},{}],"W12f":[function(require,module,exports) {
+module.exports="fog-2.f61e3d66.jpg";
+},{}],"FM5N":[function(require,module,exports) {
+module.exports="forest.17be4b14.jpg";
+},{}],"d2FE":[function(require,module,exports) {
+module.exports="fuzzy_monster.9f8a7744.jpg";
+},{}],"UO8k":[function(require,module,exports) {
+module.exports="hannah1.a72ac4c8.jpg";
+},{}],"FRty":[function(require,module,exports) {
+module.exports="hannah2.e998344e.jpg";
+},{}],"uWQ5":[function(require,module,exports) {
+module.exports="joanna1.b9bedec3.jpg";
+},{}],"IEPf":[function(require,module,exports) {
+module.exports="joanna2.7df104ec.jpg";
+},{}],"Zwum":[function(require,module,exports) {
+module.exports="joshua1.3d330e28.jpg";
+},{}],"whIA":[function(require,module,exports) {
+module.exports="joshua2.3ee2e08f.jpg";
+},{}],"VZb9":[function(require,module,exports) {
+module.exports="koreena1.ba7f39ea.jpg";
+},{}],"On0K":[function(require,module,exports) {
+module.exports="koreena2.a63c0698.jpg";
+},{}],"ws6S":[function(require,module,exports) {
+module.exports="kyle2.83b9704a.jpg";
+},{}],"f5SI":[function(require,module,exports) {
+module.exports="kyle1.317fa433.jpg";
+},{}],"mWWV":[function(require,module,exports) {
+module.exports="miyuki1.eaab9080.jpg";
+},{}],"MytZ":[function(require,module,exports) {
+module.exports="miyuki2.ca26ed8c.jpg";
+},{}],"Lvnf":[function(require,module,exports) {
+module.exports="nao1.3e042be0.jpg";
+},{}],"DOdV":[function(require,module,exports) {
+module.exports="nessy.ac364686.jpg";
+},{}],"y185":[function(require,module,exports) {
+module.exports="nao2.5fd9817d.jpg";
+},{}],"vBZm":[function(require,module,exports) {
+module.exports="raleigh2.02c9de89.jpg";
+},{}],"laB3":[function(require,module,exports) {
+module.exports="schyler2.17ab0010.jpg";
+},{}],"hpxJ":[function(require,module,exports) {
+module.exports="schyler1.b76587a8.jpg";
+},{}],"mcDD":[function(require,module,exports) {
+module.exports="raleigh1.e378af25.jpg";
+},{}],"yHmp":[function(require,module,exports) {
+module.exports="title.d4ad4c90.jpg";
+},{}],"OMFS":[function(require,module,exports) {
+module.exports="tomy2.0c178eb8.jpg";
+},{}],"SnQq":[function(require,module,exports) {
+module.exports="vivien2.2d50d2ed.jpg";
+},{}],"tY2B":[function(require,module,exports) {
+module.exports="tomy1.50502fe9.jpg";
+},{}],"elaE":[function(require,module,exports) {
+module.exports="vivien1.32fba380.jpg";
+},{}],"m7Bz":[function(require,module,exports) {
+module.exports={andruw1:require("./andruw1.jpg"),andruw2:require("./andruw2.jpg"),annika1:require("./annika1.jpg"),annika2:require("./annika2.jpg"),ariana1:require("./ariana1.jpg"),ariana2:require("./ariana2.jpg"),background:require("./background.jpg"),blue_fairy:require("./blue_fairy.jpg"),christi1:require("./christi1.jpg"),christi2:require("./christi2.jpg"),christy1:require("./christy1.jpg"),emily1:require("./emily1.jpg"),christy2:require("./christy2.jpg"),emily2:require("./emily2.jpg"),eryn1:require("./eryn1.jpg"),eryn2:require("./eryn2.jpg"),evan1:require("./evan1.jpg"),evan2:require("./evan2.jpg"),"fog-1":require("./fog-1.jpg"),"fog-2":require("./fog-2.jpg"),forest:require("./forest.jpg"),fuzzy_monster:require("./fuzzy_monster.jpg"),hannah1:require("./hannah1.jpg"),hannah2:require("./hannah2.jpg"),joanna1:require("./joanna1.jpg"),joanna2:require("./joanna2.jpg"),joshua1:require("./joshua1.jpg"),joshua2:require("./joshua2.jpg"),koreena1:require("./koreena1.jpg"),koreena2:require("./koreena2.jpg"),kyle2:require("./kyle2.jpg"),kyle1:require("./kyle1.jpg"),miyuki1:require("./miyuki1.jpg"),miyuki2:require("./miyuki2.jpg"),nao1:require("./nao1.jpg"),nessy:require("./nessy.jpg"),nao2:require("./nao2.jpg"),raleigh2:require("./raleigh2.jpg"),schyler2:require("./schyler2.jpg"),schyler1:require("./schyler1.jpg"),raleigh1:require("./raleigh1.jpg"),title:require("./title.jpg"),tomy2:require("./tomy2.jpg"),vivien2:require("./vivien2.jpg"),tomy1:require("./tomy1.jpg"),vivien1:require("./vivien1.jpg")};
+},{"./andruw1.jpg":"HvU3","./andruw2.jpg":"i341","./annika1.jpg":"KSi4","./annika2.jpg":"xiDP","./ariana1.jpg":"lH4H","./ariana2.jpg":"FJiR","./background.jpg":"p9dy","./blue_fairy.jpg":"WIW0","./christi1.jpg":"RYUG","./christi2.jpg":"mee9","./christy1.jpg":"ZLX6","./emily1.jpg":"FKNm","./christy2.jpg":"faKY","./emily2.jpg":"gkec","./eryn1.jpg":"x0jt","./eryn2.jpg":"i0yU","./evan1.jpg":"Hdkz","./evan2.jpg":"Tree","./fog-1.jpg":"YPVS","./fog-2.jpg":"W12f","./forest.jpg":"FM5N","./fuzzy_monster.jpg":"d2FE","./hannah1.jpg":"UO8k","./hannah2.jpg":"FRty","./joanna1.jpg":"uWQ5","./joanna2.jpg":"IEPf","./joshua1.jpg":"Zwum","./joshua2.jpg":"whIA","./koreena1.jpg":"VZb9","./koreena2.jpg":"On0K","./kyle2.jpg":"ws6S","./kyle1.jpg":"f5SI","./miyuki1.jpg":"mWWV","./miyuki2.jpg":"MytZ","./nao1.jpg":"Lvnf","./nessy.jpg":"DOdV","./nao2.jpg":"y185","./raleigh2.jpg":"vBZm","./schyler2.jpg":"laB3","./schyler1.jpg":"hpxJ","./raleigh1.jpg":"mcDD","./title.jpg":"yHmp","./tomy2.jpg":"OMFS","./vivien2.jpg":"SnQq","./tomy1.jpg":"tY2B","./vivien1.jpg":"elaE"}],"iNtO":[function(require,module,exports) {
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.interfaceStudents=exports.animationStudents=void 0;var e=[{name:"Evan Costa",demoreel:"demoreel",website:"http://yourname.com",room:"Room No. 1",time:"5:00–5:20 | 6:00–6:20",initialImage:"emily1.jpg",hoveredImage:"emily2.jpg"},{name:"Calvin Gacuya",demoreel:"demoreel",website:"http://yourname.com",room:"Room No. 1",time:"5:00–5:20 | 6:00–6:20",initialImage:"https://placedog.net/500",hoveredImage:"http://placekitten.com/g/200/300"},{name:"Tina Gonzalez",demoreel:"demoreel",website:"http://yourname.com",room:"Room No. 1",time:"5:00–5:20 | 6:00–6:20",initialImage:"https://placedog.net/500",hoveredImage:"http://placekitten.com/g/200/300"},{name:"Raleigh Harris",demoreel:"demoreel",website:"http://yourname.com",room:"Room No. 1",time:"5:00–5:20 | 6:00–6:20",initialImage:"https://placedog.net/500",hoveredImage:"http://placekitten.com/g/200/300"},{name:"Vivien Liu",demoreel:"demoreel",website:"http://yourname.com",room:"Room No. 1",time:"5:00–5:20 | 6:00–6:20",initialImage:"https://placedog.net/500",hoveredImage:"http://placekitten.com/g/200/300"},{name:"Nao Minegishi",demoreel:"demoreel",website:"http://yourname.com",room:"Room No. 1",time:"5:00–5:20 | 6:00–6:20",initialImage:"https://placedog.net/500",hoveredImage:"http://placekitten.com/g/200/300"},{name:"Andruw Penera",demoreel:"demoreel",website:"http://yourname.com",room:"Room No. 1",time:"5:00–5:20 | 6:00–6:20",initialImage:"https://placedog.net/500",hoveredImage:"http://placekitten.com/g/200/300"},{name:"Kyle Moy",demoreel:"demoreel",website:"http://yourname.com",room:"Room No. 1",time:"5:00–5:20 | 6:00–6:20",initialImage:"https://placedog.net/500",hoveredImage:"http://placekitten.com/g/200/300"},{name:"Joshua Richards",demoreel:"demoreel",website:"http://yourname.com",room:"Room No. 1",time:"5:00–5:20 | 6:00–6:20",initialImage:"https://placedog.net/500",hoveredImage:"http://placekitten.com/g/200/300"},{name:"Schyler Lai Shinde",demoreel:"demoreel",website:"http://yourname.com",room:"Room No. 1",time:"5:00–5:20 | 6:00–6:20",initialImage:"https://placedog.net/500",hoveredImage:"http://placekitten.com/g/200/300"},{name:"Annika Quinn",demoreel:"demoreel",website:"http://yourname.com",room:"Room No. 1",time:"5:00–5:20 | 6:00–6:20",initialImage:"https://placedog.net/500",hoveredImage:"http://placekitten.com/g/200/300"},{name:"Hannah Woodward",demoreel:"demoreel",website:"http://yourname.com",room:"Room No. 1",time:"5:00–5:20 | 6:00–6:20",initialImage:"https://placedog.net/500",hoveredImage:"http://placekitten.com/g/200/300"}];exports.animationStudents=e;var o=[{name:"Ariana Enomoto",website:"https://aritayeno.com/",room:"Room No. 1",time:"5:00–5:20 | 6:00–6:20",initialImage:"https://placedog.net/500",hoveredImage:"http://placekitten.com/g/200/300"},{name:"Emily Freeman",website:"https://eafreeman.com",room:"Room No. 1",time:"5:00–5:20 | 6:00–6:20",initialImage:"https://placedog.net/500",hoveredImage:"http://placekitten.com/g/200/300"},{name:"Christi Gonzales",website:"https://christigonzales.com/",room:"Room No. 1",time:"5:00–5:20 | 6:00–6:20",initialImage:"https://placedog.net/500",hoveredImage:"http://placekitten.com/g/200/300"},{name:"Eryn Iwanaga",website:"https://eiwanaga.com/",room:"Room No. 1",time:"5:00–5:20 | 6:00–6:20",initialImage:"https://placedog.net/500",hoveredImage:"http://placekitten.com/g/200/300"},{name:"Christy Liang",website:"https://christyzliang.com",room:"Room No. 1",time:"5:00–5:20 | 6:00–6:20"},{name:"Koreena Nagai",website:"https://knagai.com",room:"Room No. 1",time:"5:00–5:20 | 6:00–6:20"},{name:"Miyuki Sekiguchi",website:"https://miyukingseki.com",room:"Room No. 1",time:"5:00–5:20 | 6:00–6:20"},{name:"Tomy Takemura",website:"https://tomytak.com",room:"Room No. 1",time:"5:00–5:20 | 6:00–6:20"},{name:"Joanna Yee",website:"https://joannamedia.com",room:"Room No. 1",time:"5:00–5:20 | 6:00–6:20"}];exports.interfaceStudents=o;
+},{}],"VJtr":[function(require,module,exports) {
+"use strict";var e=n(require("../img/*.jpg")),t=require("./data/students");function n(e){return e&&e.__esModule?e:{default:e}}var r=document.querySelector(".animation .students-container"),a=document.querySelector(".interface .students-container"),c=Object.keys(e.default);function o(t,n){var r,a=t.name,o=t.website,u=t.room,i=t.time,s=a.split(" ")[0].toLowerCase();return c.map(function(t,n){t.includes(s+"1")&&(r=Object.values(e.default)[n])}),'<div class="student">\n                    <img class="profile" src="'.concat(r,'" />\n                    <h3 class="name">').concat(a,'</h3>\n                    <a href="').concat(o,'"><p>').concat(o,"</p></a>\n                    ").concat(n?'<a href="google.com"><p>demo reel</p></a>':"","\n                    <p><strong>").concat(u,"</strong></p>\n                    <p><strong>").concat(i,"</strong></p>\n            </div>")}function u(e,t){var n=arguments.length>2&&void 0!==arguments[2]&&arguments[2];t.map(function(t){e.innerHTML+=o(t,n)})}u(a,t.interfaceStudents),u(r,t.animationStudents,!0);var i=document.querySelectorAll(".animation .student"),s=document.querySelectorAll(".interface .student");function d(t,n){t.forEach(function(t){var n,r,a=t.querySelector("img"),o=t.querySelector(".name").innerText.split(" ")[0].toLowerCase();c.map(function(t,a){t.includes(o+"1")&&(r=Object.values(e.default)[a]),t.includes(o+"2")&&(n=Object.values(e.default)[a])}),t.addEventListener("mouseenter",function(){return a.src=n}),t.addEventListener("mouseleave",function(){return a.src=r})})}d(s,t.interfaceStudents),d(i,t.animationStudents);
+},{"../img/*.jpg":"m7Bz","./data/students":"iNtO"}]},{},["VJtr"], null)
+//# sourceMappingURL=scripts.bfd333bc.js.map
