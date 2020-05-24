bae4c11c9bfd8b9f425acb0cc113e86dc8d9f47b
tomytak <tomyt@hawaii.edu>
2020-05-24 03:23:41 -1000

    student bg responsive not showing, need to put in cave creatures

--- dist/scripts.cd665a19.js
+++ dist/scripts.cd665a19.js
@@ -352,7 +352,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54552" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51469" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
