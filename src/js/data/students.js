899f5f6d1c00eacbc95ed61bca50f59a0104e83b
lummelis <lummelis@hawaii.edu>
2020-05-29 19:37:06 -1000

    update scss

--- src/js/data/students.js
+++ src/js/data/students.js
@@ -1,111 +1,109 @@
 export const animationStudents = [
-    {
-        name: 'Evan Costa',
-        website: 'https://sites.google.com/view/evancostademoreelwebsite/home',
-        demoreel: 'https://youtu.be/wjRJw4Utf9c',
-
-    },
-    {
-        name: 'Calvin Gacuya',
-        website: 'https://sites.google.com/view/ckdemoreel/home',
-        demoreel: 'https://youtu.be/334iELe2ix8',
-    },
-    {
-        name: 'Tina Gonzalez',
-        website: 'https://tinamg6.wixsite.com/website',
-        demoreel: '	https://youtu.be/TSmpnd0XK_w',
-    },
-    {
-        name: 'Raleigh Harris',
-        website: 'https://raleharris.wixsite.com/portfolio',
-        demoreel: 'https://youtu.be/A672oXaDmzo',
-    },
-    {
-        name: 'Vivien Liu',
-        website: 'https://liuw5vi.wixsite.com/website',
-        demoreel: '	https://youtu.be/3iW5TwhxRrA',
-    },
-    {
-        name: 'Nao Minegishi',
-        website: 'https://noshishi.com/#post-36',
-        demoreel: 'https://youtu.be/Vw6f6Vol2l0',
-    },
-    {
-        name: 'Joshua Richards',
-        website: 'https://sites.google.com/view/richardsart/home',
-        demoreel: 'https://youtu.be/F5UGdC5P0ds',
-    },
-    {
-        name: 'Schyler Lai Shinde',
-        website: 'http://umiiishi.wixsite.com/schylerlaishindekcc',
-        demoreel: '	https://youtu.be/I1boIn-PtdM',
-    },
-    {
-        name: 'Annika Quinn',
-        website: 'https://annikaq.wixsite.com/2020portfolio',
-        demoreel: '	https://www.youtube.com/watch?v=JLK65i4ubiU&feature=youtu.be',
-    },
-    {
-        name: 'Hannah Woodward',
-        website: '	https://ammagok.wixsite.com/mysite',
-        demoreel: '	https://youtu.be/8dI5jHKTEZM',
-    }
-]
+	{
+		name: "Evan Costa",
+		website: "https://sites.google.com/view/evancostademoreelwebsite/home",
+		demoreel: "https://youtu.be/wjRJw4Utf9c",
+	},
+	{
+		name: "Calvin Gacuya",
+		website: "https://sites.google.com/view/ckdemoreel/home",
+		demoreel: "https://youtu.be/334iELe2ix8",
+	},
+	{
+		name: "Tina Gonzalez",
+		website: "https://tinamg6.wixsite.com/website",
+		demoreel: "	https://youtu.be/TSmpnd0XK_w",
+	},
+	{
+		name: "Raleigh Harris",
+		website: "https://raleharris.wixsite.com/portfolio",
+		demoreel: "https://youtu.be/A672oXaDmzo",
+	},
+	{
+		name: "Vivien Liu",
+		website: "https://liuw5vi.wixsite.com/website",
+		demoreel: "	https://youtu.be/3iW5TwhxRrA",
+	},
+	{
+		name: "Nao Minegishi",
+		website: "https://noshishi.com/#post-36",
+		demoreel: "https://youtu.be/Vw6f6Vol2l0",
+	},
+	{
+		name: "Joshua Richards",
+		website: "https://sites.google.com/view/richardsart/home",
+		demoreel: "https://youtu.be/F5UGdC5P0ds",
+	},
+	{
+		name: "Schyler Lai Shinde",
+		website: "http://umiiishi.wixsite.com/schylerlaishindekcc",
+		demoreel: "	https://youtu.be/I1boIn-PtdM",
+	},
+	{
+		name: "Annika Quinn",
+		website: "https://annikaq.wixsite.com/2020portfolio",
+		demoreel: "	https://www.youtube.com/watch?v=JLK65i4ubiU&feature=youtu.be",
+	},
+	{
+		name: "Hannah Woodward",
+		website: "	https://ammagok.wixsite.com/mysite",
+		demoreel: "	https://youtu.be/8dI5jHKTEZM",
+	},
+];
 
 export const interfaceStudents = [
-    {
-        name: 'Ariana Enomoto',
-        website: 'https://arianaenomoto.com',
-        room: 'Room No. 1',
-        time: '5:00–5:20 | 6:00–6:20',
-    },
-    {
-        name: 'Emily Freeman',
-        website: 'https://eafreeman.com',
-        room: 'Room No. 1',
-        time: '5:00–5:20 | 6:00–6:20',
-    },
-    {
-        name: 'Christi Gonzales',
-        website: 'https://christigonzales.com',
-        room: 'Room No. 1',
-        time: '5:00–5:20 | 6:00–6:20',
-    },
-    {
-        name: 'Eryn Iwanaga',
-        website: 'https://eiwanaga.com/portfolio',
-        room: 'Room No. 1',
-        time: '5:00–5:20 | 6:00–6:20',
-    },
-    {
-        name: 'Christy Liang',
-        website: 'https://christyzliang.com',
-        room: 'Room No. 1',
-        time: '5:00–5:20 | 6:00–6:20',
-    },
-    {
-        name: 'Koreena Nagai',
-        website: 'https://knagai.com',
-        room: 'Room No. 1',
-        time: '5:00–5:20 | 6:00–6:20',
-    },
-    {
-        name: 'Miyuki Sekiguchi',
-        website: 'https://miyukings.github.io/myportfolio-v3_2020/',
-        room: 'Room No. 1',
-        time: '5:00–5:20 | 6:00–6:20',
-    },
-    {
-        name: 'Tomy Takemura',
-        website: 'https://tomytak.com',
-        room: 'Room No. 1',
-        time: '5:00–5:20 | 6:00–6:20',
-    },
-    {
-        name: 'Joanna Yee',
-        website: 'https://joannamedia.com',
-        room: 'Room No. 1',
-        time: '5:00–5:20 | 6:00–6:20',
-    }
-]
-
+	{
+		name: "Ariana Enomoto",
+		website: "https://arianaenomoto.com",
+		room: "Room No. 1",
+		time: "5:00–5:20 | 6:00–6:20",
+	},
+	{
+		name: "Emily Freeman",
+		website: "https://eafreeman.com",
+		room: "Room No. 1",
+		time: "5:00–5:20 | 6:00–6:20",
+	},
+	{
+		name: "Christi Gonzales",
+		website: "https://christigonzales.com",
+		room: "Room No. 1",
+		time: "5:00–5:20 | 6:00–6:20",
+	},
+	{
+		name: "Eryn Iwanaga",
+		website: "https://eiwanaga.com/portfolio",
+		room: "Room No. 1",
+		time: "5:00–5:20 | 6:00–6:20",
+	},
+	{
+		name: "Christy Liang",
+		website: "https://christyzliang.com",
+		room: "Room No. 1",
+		time: "5:00–5:20 | 6:00–6:20",
+	},
+	{
+		name: "Koreena Nagai",
+		website: "https://knagai.com",
+		room: "Room No. 1",
+		time: "5:00–5:20 | 6:00–6:20",
+	},
+	{
+		name: "Miyuki Sekiguchi",
+		website: "https://miyukings.github.io/myportfolio-v3_2020/",
+		room: "Room No. 1",
+		time: "5:00–5:20 | 6:00–6:20",
+	},
+	{
+		name: "Tomy Takemura",
+		website: "https://tomytak.com",
+		room: "Room No. 1",
+		time: "5:00–5:20 | 6:00–6:20",
+	},
+	{
+		name: "Joanna Yee",
+		website: "https://joannamedia.com",
+		room: "Room No. 1",
+		time: "5:00–5:20 | 6:00–6:20",
+	},
+];
